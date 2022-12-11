3115b06c99eb4ecdf5cfe14498cd74c916dc4ccd
Liam Peters <liamjpeters@gmail.com>
2022-12-11 17:39:26 +0000

    Day 10

---
+++ Day 10 - Cathode-Ray Tube/Part 2.ps1
@@ -0,0 +1,230 @@
+# It seems like the X register controls the horizontal position of a sprite. Specifically, the 
+# sprite is 3 pixels wide, and the X register sets the horizontal position of the middle of that 
+# sprite. (In this system, there is no such thing as "vertical position": if the sprite's horizontal
+# position puts its pixels where the CRT is currently drawing, then those pixels will be drawn.)
+# 
+# You count the pixels on the CRT: 40 wide and 6 high. This CRT screen draws the top row of pixels 
+# left-to-right, then the row below that, and so on. The left-most pixel in each row is in position 
+# 0, and the right-most pixel in each row is in position 39.
+# 
+# Like the CPU, the CRT is tied closely to the clock circuit: the CRT draws a single pixel during
+# each cycle. Representing each pixel of the screen as a #, here are the cycles during which the 
+# first and last pixel in each row are drawn:
+# 
+# Cycle   1 -> ######################################## <- Cycle  40
+# Cycle  41 -> ######################################## <- Cycle  80
+# Cycle  81 -> ######################################## <- Cycle 120
+# Cycle 121 -> ######################################## <- Cycle 160
+# Cycle 161 -> ######################################## <- Cycle 200
+# Cycle 201 -> ######################################## <- Cycle 240
+
+# So, by carefully timing the CPU instructions and the CRT drawing operations, you should be able to
+# determine whether the sprite is visible the instant each pixel is drawn. If the sprite is 
+# positioned such that one of its three pixels is the pixel currently being drawn, the screen 
+# produces a lit pixel (#); otherwise, the screen leaves the pixel dark (.).
+# 
+# The first few pixels from the larger example above are drawn as follows:
+# 
+# Sprite position: ###.....................................
+# 
+# Start cycle   1: begin executing addx 15
+# During cycle  1: CRT draws pixel in position 0
+# Current CRT row: #
+# 
+# During cycle  2: CRT draws pixel in position 1
+# Current CRT row: ##
+# End of cycle  2: finish executing addx 15 (Register X is now 16)
+# Sprite position: ...............###......................
+# 
+# Start cycle   3: begin executing addx -11
+# During cycle  3: CRT draws pixel in position 2
+# Current CRT row: ##.
+# 
+# During cycle  4: CRT draws pixel in position 3
+# Current CRT row: ##..
+# End of cycle  4: finish executing addx -11 (Register X is now 5)
+# Sprite position: ....###.................................
+# 
+# Start cycle   5: begin executing addx 6
+# During cycle  5: CRT draws pixel in position 4
+# Current CRT row: ##..#
+# 
+# During cycle  6: CRT draws pixel in position 5
+# Current CRT row: ##..##
+# End of cycle  6: finish executing addx 6 (Register X is now 11)
+# Sprite position: ..........###...........................
+# 
+# Start cycle   7: begin executing addx -3
+# During cycle  7: CRT draws pixel in position 6
+# Current CRT row: ##..##.
+# 
+# During cycle  8: CRT draws pixel in position 7
+# Current CRT row: ##..##..
+# End of cycle  8: finish executing addx -3 (Register X is now 8)
+# Sprite position: .......###..............................
+# 
+# Start cycle   9: begin executing addx 5
+# During cycle  9: CRT draws pixel in position 8
+# Current CRT row: ##..##..#
+# 
+# During cycle 10: CRT draws pixel in position 9
+# Current CRT row: ##..##..##
+# End of cycle 10: finish executing addx 5 (Register X is now 13)
+# Sprite position: ............###.........................
+# 
+# Start cycle  11: begin executing addx -1
+# During cycle 11: CRT draws pixel in position 10
+# Current CRT row: ##..##..##.
+# 
+# During cycle 12: CRT draws pixel in position 11
+# Current CRT row: ##..##..##..
+# End of cycle 12: finish executing addx -1 (Register X is now 12)
+# Sprite position: ...........###..........................
+# 
+# Start cycle  13: begin executing addx -8
+# During cycle 13: CRT draws pixel in position 12
+# Current CRT row: ##..##..##..#
+# 
+# During cycle 14: CRT draws pixel in position 13
+# Current CRT row: ##..##..##..##
+# End of cycle 14: finish executing addx -8 (Register X is now 4)
+# Sprite position: ...###..................................
+# 
+# Start cycle  15: begin executing addx 13
+# During cycle 15: CRT draws pixel in position 14
+# Current CRT row: ##..##..##..##.
+# 
+# During cycle 16: CRT draws pixel in position 15
+# Current CRT row: ##..##..##..##..
+# End of cycle 16: finish executing addx 13 (Register X is now 17)
+# Sprite position: ................###.....................
+# 
+# Start cycle  17: begin executing addx 4
+# During cycle 17: CRT draws pixel in position 16
+# Current CRT row: ##..##..##..##..#
+# 
+# During cycle 18: CRT draws pixel in position 17
+# Current CRT row: ##..##..##..##..##
+# End of cycle 18: finish executing addx 4 (Register X is now 21)
+# Sprite position: ....................###.................
+# 
+# Start cycle  19: begin executing noop
+# During cycle 19: CRT draws pixel in position 18
+# Current CRT row: ##..##..##..##..##.
+# End of cycle 19: finish executing noop
+# 
+# Start cycle  20: begin executing addx -1
+# During cycle 20: CRT draws pixel in position 19
+# Current CRT row: ##..##..##..##..##..
+# 
+# During cycle 21: CRT draws pixel in position 20
+# Current CRT row: ##..##..##..##..##..#
+# End of cycle 21: finish executing addx -1 (Register X is now 20)
+# Sprite position: ...................###..................
+# Allowing the program to run to completion causes the CRT to produce the following image:
+# 
+#..##..##..##..##..##..##..##..##..##..
+##...###...###...###...###...###...###.
+###....####....####....####....####....
+####.....#####.....#####.....#####.....
+#####......######......######......####
+######.......#######.......#######.....
+
+# Render the image given by your program. What eight capital letters appear on your CRT?
+
+class CRT {
+    [int32] $CurrentPixel = 0
+
+    [int32] $Width = 40
+    [int32] $Height = 6
+    [string[]] $Pixels = @()
+
+    [string] $Empty = '.'
+    [string] $Filled = '#'
+
+    [int32] $SpriteLocation = 0
+
+    CRT() {}
+
+    [void] SetSpriteLocation([int32] $Location) {
+        $this.SpriteLocation = $Location
+    }
+
+    [void] TickClock() {
+        $Pix = $this.CurrentPixel % $this.Width
+        if ($Pix -in (($this.SpriteLocation - 1)..($this.SpriteLocation + 1))) {
+            $this.Pixels += $this.Filled
+        } else {
+            $this.Pixels += $this.Empty
+        }
+        $this.CurrentPixel++
+    }
+
+    [void] Print() {
+        for ($i = 0; $i -lt $this.Pixels.Count; $i++) {
+            if ($i % $this.Width -eq 0) {
+                Write-Host "`n" -NoNewline
+            }
+            Write-Host $this.Pixels[$i] -NoNewline
+        }
+        Write-Host "`n" -NoNewline
+    }
+
+}
+
+class CPU {
+
+    [int32] $ProgramClock = 0
+    [int32] $RegisterX = 1
+
+    [CRT] $CRT = [CRT]::new()
+
+    CPU() {}
+
+    [void] TickClock([int32] $Ticks) {
+        foreach($Num in 1..$Ticks) {
+            $this.CRT.TickClock()
+            $this.ProgramClock++
+        }
+    }
+
+    [void] ProcessInstructions ([string[]] $Instructions) {
+        foreach($Instruction in $Instructions) {
+            $SplitInst = $Instruction.Split(' ')
+            $Command = $SplitInst[0]
+            $Arguments = $SplitInst[1] -as [int32]
+            switch ($Command) {
+                'addX' { 
+                    $this.TickClock(2)
+                    $this.RegisterX += $Arguments
+                    $this.CRT.SetSpriteLocation($this.RegisterX)
+                    break
+                }
+                'noop' {
+                    $this.TickClock(1)
+                    break
+                }
+                Default {}
+            }
+        }
+    }
+
+    [void] PrintScreen() {
+        $this.CRT.Print()
+    }
+
+}
+
+function New-AoCCPU {
+    param (
+    )
+    [CPU]::new()
+}
+
+$Instructions = . "$PSScriptRoot\Inputs.ps1"
+
+$CPU = New-AoCCPU
+
+$CPU.ProcessInstructions($Instructions)
+
+$CPU.PrintScreen()
